717baa738549dc5072bb2a8d9a94aab7362defac
Hiroaki <buyselljapan0311@gmail.com>
2026-02-20 05:28:40 +0900

    new

--- app/root.tsx
+++ app/root.tsx
@@ -77,6 +77,7 @@ export default function App() {
             <head>
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="google-site-verification" content="uhVyFz8gxRZ70zYu7xldgnJBNE89v5cASmrKSlzs05o" />
                 <Meta />
                 <Links />
             </head>
